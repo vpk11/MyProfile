263c263549beda2b6dd999e4eb45181508811179
VISHNUPRASAD K <vishnuemerges@gmail.com>
2019-01-11 16:10:07 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png" /> <MainProfile />
+  </div>
+</template>
+
+<script>
+import MainProfile from "./components/MainProfile";
+
+export default {
+  name: "App",
+  components: {
+    MainProfile
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MainProfile.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1>
+    <h2>{{ designation }}</h2>
+    <ul>
+      <li v-for="socialLink in socialLinks">
+        <a v-bind:href="socialLink.link" target="_blank">
+          {{ socialLink.name }}
+        </a>
+      </li>
+    </ul>
+    <h3>In Love With</h3>
+    <ul>
+      <li v-for="language in languages">{{ language.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainProfile",
+  data() {
+    return {
+      name: "Vishnu Prasad K",
+      designation: "Software Engineer",
+      socialLinks: [
+        { name: "Github", link: "https://github.com/vpk11" },
+        { name: "Twitter", link: "https://twitter.com/vpk11" }
+      ],
+      languages: [
+        { name: "Ruby" },
+        { name: "JavaScript" },
+        { name: "Android Development" }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
